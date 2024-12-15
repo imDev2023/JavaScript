4535eb4e6abde54b1f7f3f622fbcf214d97d91d7
Farhan Basharat <141978959+imDev2023@users.noreply.github.com>
2024-12-15 04:12:36 +0000

    countdown timer

---
+++ 11-countdown-timer/script1.js
@@ -0,0 +1,27 @@
+const newYears = "1 Jan 2024";
+
+function countDown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) /1000;
+
+    const days = (Math.floor(totalSeconds/3600/24));
+    const hours = Math.floor(totalSeconds/3600) % 24;
+    const minutes = (Math.floor(totalSeconds/60) % 60);
+    const seconds = (Math.floor(totalSeconds % 60));
+
+    document.getElementById('days').innerHTML=formatTime(days);
+    document.getElementById('hours').innerHTML = formatTime(hours);
+    document.getElementById('mins').innerHTML = formatTime(minutes);
+    document.getElementById('seconds').innerHTML = formatTime(seconds);
+}
+
+function formatTime(time){
+    return time <10 ? `0${time}` : time;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
